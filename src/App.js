12043c42738e9931b432ba3760b8f2141251a763
axel-ac <abdullahceylan.axel@gmail.com>
2023-06-12 17:36:31 +0300

    finished.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import "./styles.css";
 function App() {
 
   const [minVal, setMinVal] = useState(0)
-  const [maxVal, setMaxVal] = useState(10)
-  const [randomNum, setRandomNum] = useState(5)
+  const [maxVal, setMaxVal] = useState(5)
+  const [randomNum, setRandomNum] = useState(0)
 
   const handleRandomNum = (e) => {
-    setRandomNum(Math.floor(Math.random() * (maxVal - minVal) + minVal))
+    setRandomNum(Math.floor(Math.random() * (maxVal - minVal + 1) + minVal))
   }
 
   return (
