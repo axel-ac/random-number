8e7b38d85aa6006c06d09940c27057571c727907
axel-ac <abdullahceylan.axel@gmail.com>
2023-06-12 14:00:01 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import "./styles.css";
 function App() {
   return (
     <div className="hero">
-      Hi!
+      <div className="container">
+        <div className="randomNum">
+          <p>
+            Random Number <span>0</span>
+          </p>
+        </div>
+        <div className="numContainer">
+          <div>
+            <p>Min:</p>
+            <input type="number" value={0} />
+          </div>
+          <div>
+            <p>Max:</p>
+            <input type="number" value={0} />
+          </div>
+        </div>
+        <button>Get Random Number</button>
+      </div>
     </div>
   );
 }
